43c1fdb181bdac7c2ce87c4fb3a364562b870f81
Jefferson Castillo <jeffersonk20castillo@gmail.com>
2026-09-23 16:57:05 -0600

    Se creo la busqueda y eliminar, modificar clientes

--- conexion.py
+++ conexion.py
@@ -44,3 +44,4 @@ class Conexion:
             return f'Error: {e}'
     
         
+    
--- server.py
+++ server.py
@@ -26,15 +26,16 @@ class miServidor(SimpleHTTPRequestHandler):
         urlParse = urlparse(self.path)
         qs = parse_qs(urlParse.query)
        
-        if urlParse.path == "/saludo":
-            saludo = qs["nombre"][0] + " bienvenido a Python"
-            
+        if urlParse.path == "/clientes":
+            buscar = qs.get('buscar', [''])[0]
+            print(buscar)
+            datos = crudClientes.consultar(buscar)
             self.send_response(200)
-            self.send_header("Content-type","text/html")
+            self.send_header("Content-type","text/json")
             self.end_headers()
-            self.wfile.write(saludo.encode("utf-8"))
-
-        if self.path == "/":
+            self.wfile.write(json.dumps(datos).encode("utf-8"))
+        
+        elif self.path == "/":
             self.path = "/index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
 
